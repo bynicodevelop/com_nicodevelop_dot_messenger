f69a479909f99464cd829b43f7c7054a52310abd
nico-develop <contact@nico-develop.fr>
2022-11-05 11:49:13 +0100

    Permet d'intégrer les groups en relation avec les utilisateurs de la recherche

--- lib/repositories/search_repository.dart
+++ lib/repositories/search_repository.dart
@@ -1,16 +1,29 @@
 import "package:cloud_firestore/cloud_firestore.dart";
+import "package:com_nicodevelop_dotmessenger/exceptions/authentication_exception.dart";
 import "package:com_nicodevelop_dotmessenger/exceptions/search_exception.dart";
 import "package:com_nicodevelop_dotmessenger/utils/logger.dart";
+import "package:firebase_auth/firebase_auth.dart";
 import "package:validators/sanitizers.dart";
 
 class SearchRepository {
   final FirebaseFirestore firestore;
+  final FirebaseAuth auth;
 
   const SearchRepository(
     this.firestore,
+    this.auth,
   );
 
   Future<List<Map<String, dynamic>>> search(Map<String, dynamic> data) async {
+    final User? user = auth.currentUser;
+
+    if (user == null) {
+      throw const AuthenticationException(
+        "User not connected",
+        "unauthenticated",
+      );
+    }
+
     if (!data.containsKey("query")) {
       warn("data does not contain key 'query'");
 
@@ -31,11 +44,24 @@ class SearchRepository {
         .orderBy("displayName")
         .startAt([query]).endAt(["$query\uf8ff"]).get();
 
-    return snapshot.docs.map((QueryDocumentSnapshot<Map<String, dynamic>> doc) {
-      return {
+    return (await Future.wait(snapshot.docs
+            .map((QueryDocumentSnapshot<Map<String, dynamic>> doc) async {
+      final QuerySnapshot<Map<String, dynamic>> groupSnapshot = await firestore
+          .collection("groups")
+          .where("users", arrayContainsAny: [user.uid, doc.id]).get();
+
+      final Map<String, dynamic> profile = {
         "uid": doc.id,
         "displayName": doc.data()["displayName"],
       };
-    }).toList();
+
+      if (groupSnapshot.docs.isNotEmpty) {
+        profile["groupId"] = groupSnapshot.docs.first.id;
+      }
+
+      return profile;
+    })))
+        .where((profile) => profile["uid"] != user.uid)
+        .toList();
   }
 }
--- test/repositories/search_repository_test.dart
+++ test/repositories/search_repository_test.dart
@@ -1,14 +1,22 @@
 import "package:cloud_firestore/cloud_firestore.dart";
+import "package:com_nicodevelop_dotmessenger/exceptions/authentication_exception.dart";
 import "package:com_nicodevelop_dotmessenger/exceptions/search_exception.dart";
 import "package:com_nicodevelop_dotmessenger/repositories/search_repository.dart";
 import "package:fake_cloud_firestore/fake_cloud_firestore.dart";
+import "package:firebase_auth/firebase_auth.dart";
+import "package:firebase_auth_mocks/firebase_auth_mocks.dart";
 import "package:flutter_test/flutter_test.dart";
 
 void main() {
   group("search", () {
     final FirebaseFirestore firestore = FakeFirebaseFirestore();
+    late FirebaseAuth auth;
 
     setUp(() async {
+      auth = MockFirebaseAuth(
+        signedIn: true,
+      );
+
       final List<Map<String, dynamic>> users = [
         {
           "uid": "1",
@@ -22,16 +30,56 @@ void main() {
           "uid": "3",
           "displayName": "john",
         },
+        {
+          "uid": "4",
+          "displayName": "jeff",
+        },
+        {
+          "uid": "5",
+          "displayName": "dolly",
+        },
+        {
+          "uid": "6",
+          "displayName": "pit",
+        },
+        {
+          "uid": "7",
+          "displayName": "pat",
+        }
       ];
 
       for (final Map<String, dynamic> user in users) {
         await firestore.collection("users").doc(user["uid"]).set(user);
       }
+
+      final List<Map<String, dynamic>> groups = [
+        {
+          "uid": "group1",
+          "users": ["4", "5"],
+        },
+        {
+          "uid": "group2",
+          "users": ["4", "6"],
+        },
+        {
+          "uid": "group3",
+          "users": ["5", "7"],
+        },
+      ];
+
+      for (final Map<String, dynamic> group in groups) {
+        await firestore.collection("groups").doc(group["uid"]).set({
+          "users": group["users"],
+        });
+      }
     });
 
     test("Doit retourner une liste vide", () async {
       // ARRANGE
-      final SearchRepository searchRepository = SearchRepository(firestore);
+      final SearchRepository searchRepository = SearchRepository(
+        firestore,
+        auth,
+      );
 
       // ACT
       final List<Map<String, dynamic>> result = await searchRepository.search({
@@ -46,7 +94,10 @@ void main() {
       // ARRANGE
       final List<String> queries = ["    ", " sd", "s ", " ", "te"];
 
-      final SearchRepository searchRepository = SearchRepository(firestore);
+      final SearchRepository searchRepository = SearchRepository(
+        firestore,
+        auth,
+      );
 
       // ACT
       for (final String query in queries) {
@@ -62,7 +113,10 @@ void main() {
 
     test("Doit retourner une liste vide avec 1 résultat", () async {
       // ARRANGE
-      final SearchRepository searchRepository = SearchRepository(firestore);
+      final SearchRepository searchRepository = SearchRepository(
+        firestore,
+        auth,
+      );
 
       // ACT
       final List<Map<String, dynamic>> result = await searchRepository.search({
@@ -80,7 +134,10 @@ void main() {
 
     test("Doit retourner une liste vide avec 2 résultats", () async {
       // ARRANGE
-      final SearchRepository searchRepository = SearchRepository(firestore);
+      final SearchRepository searchRepository = SearchRepository(
+        firestore,
+        auth,
+      );
 
       // ACT
       final List<Map<String, dynamic>> result = await searchRepository.search({
@@ -100,10 +157,97 @@ void main() {
       ]);
     });
 
+    test("Doit retourner une erreur si l'utilisateur n'est pas connecté",
+        () async {
+      // ARRANGE
+      auth = MockFirebaseAuth(
+        signedIn: false,
+      );
+
+      final SearchRepository searchRepository = SearchRepository(
+        firestore,
+        auth,
+      );
+
+      // ACT
+      // ASSERT
+      expect(
+          () async => await searchRepository.search({"query": ""}),
+          throwsA(
+            isA<AuthenticationException>().having(
+              (p0) => p0.code,
+              "code",
+              "unauthenticated",
+            ),
+          ));
+    });
+
+    test(
+        "Doit retourner une liste vide avec 1 résultat qui contient un groupe en commun",
+        () async {
+      // ARRANGE
+      final MockUser user = MockUser(
+        uid: "4",
+      );
+
+      final FirebaseAuth auth = MockFirebaseAuth(
+        signedIn: true,
+        mockUser: user,
+      );
+
+      final SearchRepository searchRepository = SearchRepository(
+        firestore,
+        auth,
+      );
+
+      // ACT
+      final List<Map<String, dynamic>> result = await searchRepository.search({
+        "query": "dolly",
+      });
+
+      // ASSERT
+      expect(result, [
+        {
+          "uid": "5",
+          "displayName": "dolly",
+          "groupId": "group1",
+        },
+      ]);
+    });
+
+    test("Doit exclure l'utilisateur courant de la liste des résultats",
+        () async {
+      // ARRANGE
+      final MockUser user = MockUser(
+        uid: "4",
+      );
+
+      final FirebaseAuth auth = MockFirebaseAuth(
+        signedIn: true,
+        mockUser: user,
+      );
+
+      final SearchRepository searchRepository = SearchRepository(
+        firestore,
+        auth,
+      );
+
+      // ACT
+      final List<Map<String, dynamic>> result = await searchRepository.search({
+        "query": "jeff",
+      });
+
+      // ASSERT
+      expect(result, []);
+    });
+
     test("Doit retourner une erreur si la donnéee query n'est pas renseignée",
         () async {
       // ARRANGE
-      final SearchRepository searchRepository = SearchRepository(firestore);
+      final SearchRepository searchRepository = SearchRepository(
+        firestore,
+        auth,
+      );
 
       // ACT
       // ASSERT
