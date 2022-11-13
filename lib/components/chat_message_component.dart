76ad747c0ddb9d5d0fba0302c7e0e6b8aebeffa4
nico-develop <contact@nico-develop.fr>
2022-11-13 14:41:29 +0100

    modiffication du nom d'un fichier

--- lib/components/chat_message_component.dart
+++ lib/components/chat_message_component.dart
@@ -1,7 +1,7 @@
 import "package:com_nicodevelop_dotmessenger/components/bubble_event_wrapper_component.dart";
 import "package:com_nicodevelop_dotmessenger/components/empty_wrapper_component.dart";
 import "package:com_nicodevelop_dotmessenger/components/responsive_component.dart";
-import "package:com_nicodevelop_dotmessenger/components/skeletons/chat_skeletongs_component.dart";
+import "package:com_nicodevelop_dotmessenger/components/skeletons/chat_skeletons_component.dart";
 import "package:com_nicodevelop_dotmessenger/services/chat/load_messages/load_messages_bloc.dart";
 import "package:com_nicodevelop_dotmessenger/services/chat/post_message/post_message_bloc.dart";
 import "package:com_nicodevelop_dotmessenger/services/groups/open_group/open_group_bloc.dart";
